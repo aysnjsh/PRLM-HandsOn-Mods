5778be2f36766daac858f5d2857ad1183a7703ad
aysnjsh <sensubmissions@gmail.com>
2025-11-30 20:13:56 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+echo "<hr>";
+echo "<p style='text-align:center; font-size:14px;'>
+        Haikyuu-Themed Food Place
+      </p>";
+echo "<p style='text-align:center; font-size:12px; color:gray;'>
+        © " . date("Y") . " Spike-Eat In
+      </p>";
+?>
---
+++ header.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Spike-Eat In</h1>";
+echo "<p>Eat with Spikes in your stomach.</p><hr>";
+?>
